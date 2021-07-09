7a499febcca844bc6795d143929d0b3da4100162
白袍 <18795121826@163.com>
2021-07-09 17:15:01 +0800

    更新了主函数

--- src/main/java/com/equations/intelliorder/IntelliorderApplication.java
+++ src/main/java/com/equations/intelliorder/IntelliorderApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IntelliorderApplication {
 
     public static void main(String[] args) {
+        //主程序入口
         SpringApplication.run(IntelliorderApplication.class, args);
 
     }
