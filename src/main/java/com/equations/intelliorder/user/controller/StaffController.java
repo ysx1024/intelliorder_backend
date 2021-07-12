e5c91f22fe5ff09822555bddce67d75287734811
白袍 <18795121826@163.com>
2021-07-12 16:03:53 +0800

    更新了增加与模糊查找

--- src/main/java/com/equations/intelliorder/user/controller/StaffController.java
+++ src/main/java/com/equations/intelliorder/user/controller/StaffController.java
@@ -5,7 +5,7 @@ import com.alibaba.fastjson.JSON;
 import com.equations.intelliorder.user.entity.Staff;
 import com.equations.intelliorder.user.service.IStaffService;
 import io.swagger.annotations.*;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,9 +29,11 @@ import java.util.Map;
 
 public class StaffController {
 
-    @Autowired
-    private IStaffService staffService;
+    private final IStaffService staffService;
 
+    public StaffController(IStaffService staffService) {
+        this.staffService = staffService;
+    }
 
 
     @RequestMapping(value = "/getStaffById",method = RequestMethod.GET)
@@ -46,10 +48,24 @@ public class StaffController {
             @ApiResponse(code=200,message="请求成功")
     })
 
-    public String getStaffById(String id) {
-        Map<String,Object> map =new HashMap<String,Object>();
+    public String getStaffById(int id) {
+        Map<String,Object> map =new HashMap<>();
         try {
             List<Staff> staffList = staffService.getStaffById(id);
+            map.put("status", "200");
+            map.put("data", staffList);
+        }catch (Exception exception){
+            map.put("status", "20001");
+            map.put("errorMsg", exception.getMessage());
+        }
+        return JSON.toJSONString(map);
+    }
+
+    @RequestMapping(value = "/getStaffByName",method = RequestMethod.GET)
+        public String getStaffByName(String name) {
+            Map<String,Object> map =new HashMap<>();
+        try {
+            List<Staff> staffList = staffService.getStaffByName(name);
             map.put("status", "200");
             map.put("data", staffList);
         }catch (Exception exception){
@@ -63,7 +79,7 @@ public class StaffController {
     public String updateStaff(
             int id, String phone, String account,
             String password, String staffType){
-        Map<String,Object> map = new HashMap<String,Object>();
+        Map<String,Object> map = new HashMap<>();
         try {
             int result=staffService.updateStaff(id,phone,account,password,staffType);
             if(result==1){
@@ -80,9 +96,31 @@ public class StaffController {
         return JSON.toJSONString(map);
     }
 
+
+    @RequestMapping(value = "/addStaff",method = RequestMethod.POST)
+    public String addStaff(
+            String name, String phone, String account,
+            String password, String staffType) {
+        Map<String, Object> map = new HashMap<>();
+        try {
+            int result = staffService.addStaff(name, phone, account, password, staffType);
+            if (result == 1) {
+                map.put("status", "200");
+                map.put("msg", "添加成功");
+            } else {
+                map.put("status", "404");
+                map.put("msg", "添加失败");
+            }
+        } catch (Exception exception) {
+            map.put("status", "-1");
+            map.put("errorMsg", exception.getMessage());
+        }
+        return JSON.toJSONString(map);
+    }
+
     @RequestMapping(value = "/deleteById",method = RequestMethod.DELETE)
-    public String deleteById(String id){
-        Map<String,Object> map = new HashMap<String,Object>();
+    public String deleteById(int id){
+        Map<String,Object> map = new HashMap<>();
         try {
             int result=staffService.deleteById(id);
             if(result==1){
