8305326b2f450c747bc24765676905ccf9c6dfe4
hmf <978455283@qq.com>
2021-07-15 15:57:28 +0800

    胡媚凤厨师查看待做列表

---
+++ src/main/java/com/equations/intelliorder/order/mapper/OrderlistMapper.java
@@ -0,0 +1,18 @@
+package com.equations.intelliorder.order.mapper;
+
+import com.equations.intelliorder.order.entity.Orderlist;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author equations
+ * @since 2021-07-14
+ */
+@Mapper
+public interface OrderlistMapper extends BaseMapper<Orderlist> {
+
+}
---
+++ src/main/java/com/equations/intelliorder/order/service/IOrderlistService.java
@@ -0,0 +1,21 @@
+package com.equations.intelliorder.order.service;
+
+import com.equations.intelliorder.order.entity.Orderlist;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.equations.intelliorder.user.entity.Staff;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author equations
+ * @since 2021-07-14
+ */
+public interface IOrderlistService extends IService<Orderlist> {
+
+    List<Orderlist> showOrderlistList();//返回待做菜品列表
+
+}
